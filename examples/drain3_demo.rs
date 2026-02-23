babbebbbc9ea9bb00af92d38ea710d790c70ef74
sanekee <5906708+sanekee@users.noreply.github.com>
2026-02-23 07:10:34 +0000

    feat: Regex

--- examples/drain3_demo.rs
+++ examples/drain3_demo.rs
@@ -67,19 +67,19 @@ fn main() -> anyhow::Result<()> {
 
     println!("Processing {}...", &log_file_name);
 
-    // let guard = pprof::ProfilerGuardBuilder::default()
-    //     .frequency(1000)
-    //     .blocklist(&[
-    //         "libc",
-    //         "libgcc",
-    //         "pthread",
-    //         "vdso",
-    //         "flamegraph",
-    //         "pprof",
-    //         "inferno",
-    //     ])
-    //     .build()
-    //     .unwrap();
+    let guard = pprof::ProfilerGuardBuilder::default()
+        .frequency(1000)
+        .blocklist(&[
+            "libc",
+            "libgcc",
+            "pthread",
+            "vdso",
+            "flamegraph",
+            "pprof",
+            "inferno",
+        ])
+        .build()
+        .unwrap();
 
     for line in reader.lines() {
         let line_num = line_count + 1;
@@ -111,14 +111,6 @@ fn main() -> anyhow::Result<()> {
             });
         }
 
-        // if let Ok(report) = guard.report().build() {
-        //     let file = File::create("examples/outputs/flamegraph.svg").unwrap();
-        //     let mut options = pprof::flamegraph::Options::default();
-        //     options.image_width = Some(3000);
-        //     report.flamegraph_with_options(file, &mut options).unwrap();
-        //     // report.flamegraph(file).unwrap();
-        // }
-
         line_count += 1;
         if line_count % 10000 == 0 {
             break;
@@ -131,6 +123,14 @@ fn main() -> anyhow::Result<()> {
             );
             batch_start = now;
         }
+    }
+
+    if let Ok(report) = guard.report().build() {
+        let file = File::create("examples/outputs/flamegraph.svg").unwrap();
+        let mut options = pprof::flamegraph::Options::default();
+        options.image_width = Some(3000);
+        report.flamegraph_with_options(file, &mut options).unwrap();
+        // report.flamegraph(file).unwrap();
     }
 
     let duration = start.elapsed();
